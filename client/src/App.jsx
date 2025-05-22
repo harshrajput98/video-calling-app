06b592e408b72712c946a777fbc1cd322fd6768f
Harsh Kumar <116553146+harshrajput98@users.noreply.github.com>
2025-05-23 04:17:46 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import AuthForm from './pages/auth/Auth';
 import Dashboard from './pages/dashboard/Dashboard';
 import IsLogin from './pages/auth/IsLogin';
